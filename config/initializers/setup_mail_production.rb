b7b04e30dc28752ef02f71593615d9433badd998
seucroch <pablo.seuc.rocher@gmail.com>
2013-10-02 17:50:26 -0700

    sendgrid username error

--- config/initializers/setup_mail_production.rb
+++ config/initializers/setup_mail_production.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['app18327114@heroku.com'],
-    :password       => ENV['j6k4e5ks'],
+    :authentication => :plain,    
+    :user_name      => 'app18327114@heroku.com',
+    :password       => 'j6k4e5ks',
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
