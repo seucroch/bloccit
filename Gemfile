3a7fdff74407044f3c4f2705ff00b65cad552c4a
seucroch <pablo.seuc.rocher@gmail.com>
2013-10-09 17:16:02 -0700

    Added image to Post.

--- Gemfile
+++ Gemfile
@@ -37,6 +37,17 @@ gem 'devise'
 
 gem 'cancan'
 
+gem 'redcarpet'
+
+gem 'carrierwave'
+
+gem 'mini_magick'
+
+gem 'fog'
+
+gem 'figaro'
+
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131001171320) do
+ActiveRecord::Schema.define(:version => 20131009001250) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(:version => 20131001171320) do
     t.datetime "updated_at", :null => false
     t.integer  "user_id"
     t.integer  "topic_id"
+    t.string   "image"
   end
 
   add_index "posts", ["topic_id"], :name => "index_posts_on_topic_id"
@@ -61,6 +62,7 @@ ActiveRecord::Schema.define(:version => 20131001171320) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "role"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
