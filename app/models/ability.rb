3a46f7d44c7e61b32d7a952017deec7f2050d75f
seucroch <pablo.seuc.rocher@gmail.com>
2013-10-22 16:45:44 -0700

    Interlude changes for registration, email permissions and styling

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,6 +11,7 @@ class Ability
       can :manage, Comment, :user_id => user.id
       can :create, Vote
       can :manage, Favorite, user_id: user.id
+      can :read, Topic
     end
 
     # Moderators can delete any post
@@ -25,5 +26,7 @@ class Ability
     end
 
     can :read, :all
+    can :read, Topic, public: true
+    can :read, Post
   end
 end
