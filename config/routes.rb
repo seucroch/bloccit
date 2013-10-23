4a4c8d6202eb5f01d6dcf5cc13ca0f3443b1ba19
seucroch <pablo.seuc.rocher@gmail.com>
2013-10-23 15:33:14 -0700

    Created Popular Posts and Users, and organized topic#show with eager loading, even though it did seem to speed up the process.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Bloccit::Application.routes.draw do
 
+  get "posts/index"
+
   get "comments/new"
 
   get "topics/new"
 
+  resources :posts, only: [:index]
+
   resources :topics do
-    resources :posts, except: [:index] do
+    resources :posts, except: [:index], controller: 'topics/posts' do
       resources :comments, only: [:create, :destroy]
       match '/up-vote', to: 'votes#up_vote', as: :up_vote
       match '/down-vote', to: 'votes#down_vote', as: :down_vote
@@ -15,7 +19,9 @@ Bloccit::Application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
   
-  resources :users, only: [:show] # create a route for users#show
+  resources :users, only: [:show, :index]
+  
+  
 
   match "about" => 'welcome#about', via: :get
 
